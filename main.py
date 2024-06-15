4885ebe8674f791a4d88533fffa9aa84ad73b4f0
jawnthan <johnwwwhittaker@gmail.com>
2024-06-14 20:01:35 -0400

    added code to print Frankenstein to stdout

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-      file_contents = f.read()
-      print(file_contents)
-      
+  book_path = "books/frankenstein.txt"
+  text = get_book(book_path)
+  print(text)
 
-if __name__ == "__main__":
-   main()
+def get_book(path):
+  with open(path) as f:
+    return f.read()
+
+main()
