71d418e383bb25a32200c36f76d102b2736ab996
jawnthan <johnwwwhittaker@gmail.com>
2024-06-19 20:24:13 -0400

    added code for character count

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
   book_path = "books/frankenstein.txt"
   text = get_book(book_path)
   num_words = word_count(text)
+  how_many = char_count(text)
   print(text)
   print(num_words)
+  print(how_many)
 
 def get_book(path):
   with open(path) as f:
@@ -21,6 +23,19 @@ def word_count(text):
 
 def char_count(text):
 
-  for i in range()
-
+  chars = {}
+  lower_text = text.lower()
+  individuals = list(lower_text)
+  
+  for c in individuals:
+      
+      if c in chars:
+  
+          chars[c] += 1
+      
+      else:
+          chars[c] = 1
+          
+  return chars
+  
 main()
